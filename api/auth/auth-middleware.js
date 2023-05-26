b1dd3fe73c7b4bcf6b999d47acf5fb4bbb7bc8e2
Anthony Climer <anthonyclimer0925@gmail.com>
2023-05-26 19:40:52 -0400

    I have the routers fleshed out and the middlewares for the routers done

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,53 @@
+const Auth = require('./auth-model')
+
+const checkUsernameExists =  async (req, res, next) => {
+    const [user] = await Auth.findByUsername(req.body.username)
+    console.log(user)
+    try {
+        if(user){
+            next({status:422, message:"Username taken"})
+        } else{
+            req.user = user
+            next()
+        }
+    }
+    catch (err){
+        next(err)
+    }
+}
+
+const checkUsernameInDb =  async (req, res, next) => {
+    const [user] = await Auth.findByUsername(req.body.username)
+    console.log(user)
+    try {
+        if(user){
+            req.user = user
+            next()
+        } else{
+            next({status:401, message:'Invalid credentials'})
+        }
+    }
+    catch (err){
+        next(err)
+    }
+}
+
+const checkBodyInfo = (req, res, next) => {
+    const {username, password} = req.body
+    if(!username || !password){
+        res.status(401).json({message: 'username and password are required'})
+    }
+    next()
+}
+
+const checkCredentials = (req, res, next) => {
+
+    next()
+}
+
+module.exports = {
+    checkBodyInfo, 
+    checkUsernameExists, 
+    checkCredentials,
+    checkUsernameInDb
+}
--- api/jokes/jokes-router.js
+++ api/jokes/jokes-router.js
@@ -1,9 +1,31 @@
 // do not make changes to this file
 const router = require('express').Router();
 const jokes = require('./jokes-data');
+const Auth = require('../auth/auth-model')
 
 router.get('/', (req, res) => {
   res.status(200).json(jokes);
 });
+// router.get('/:id', (req, res, next) => {
+//   Auth.getId(req.params.id)
+//     .then(result => {
+//       res.status(200).json(result)
+//     })
+//     .catch(next)
+// })
+// router.post('/', (req, res, next) => {
+//   Auth.createAccount(req.body)
+//     .then(result => {
+//       res.status(201).json(result)
+//     })
+//     .catch(next)
+// })
+// router.delete('/:id', (req, res, next) =>{
+//   Auth.deleteUser(req.params.id)
+//     .then(result => {
+//       res.json({message: 'The user has been deleted'})
+//     })
+//     .catch(next)
+// })
 
 module.exports = router;
